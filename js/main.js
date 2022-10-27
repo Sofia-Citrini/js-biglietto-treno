792504bccde9f24afe016e9608f494f91ffa3ff3
Sofia-Citrini <sofiacitrini@gmail.com>
2022-10-27 14:54:53 +0200

    Prezzo scontato per età

--- js/main.js
+++ js/main.js
@@ -6,9 +6,18 @@ console.log("Età:", age);
 
 // prezzo del biglietto
 const prezzo = kilometri * 0.21;
-console.log ("Prezzo del biglietto:", prezzo);
 
 // sconto (20% minorenni - 40% over 65)
 if (isNaN(age)) {
-    alert("Età inserita non valida")
+    alert("Età inserita non valida");
+}
+
+if (age >=18 && age <65) {
+    console.log ("Prezzo del biglietto intero:", prezzo);
+} else if (age < 18) {
+    console.log ("Prezzo scontato del 20%:", prezzo - (prezzo * 20 / 100));
+    alert("Sconto del 20%");
+} else if (age >= 65 ) {
+    console.log ("Prezzo scontato del 40%:", prezzo * 0.6);
+    alert("Sconto del 40%");
 }
